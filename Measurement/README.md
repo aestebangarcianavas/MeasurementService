9ec12b82afae591a0f0fbe920be8d665bc458ded
anavas <ana.esteban@freenetdigital.com>
2019-08-13 09:35:36 +0200

    Set up of the project.

---
+++ Measurement/README.md
@@ -0,0 +1,9 @@
+# Measurement Service
+
+
+## Core Stack / Project Setup
+* java 11
+* [spring 2.1](https://spring.io/)
+* h2 InMemory DB
+* Maven
+* Jalopy format
---
+++ Measurement/src/test/java/com/measurement/collector/MeasurementCollectorApplicationTest.java
@@ -0,0 +1,22 @@
+package com.measurement.collector;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Controller to manage the operations related to the car.
+ *
+ * @author Ana Esteban Garcia-Navas(<a href="mailto:ramoni.esteban@gmail.com">ramoni</a>)
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest()
+@ActiveProfiles("unittest")
+public class MeasurementCollectorApplicationTest {
+
+    @Test
+    public void contextLoads() { }
+}
